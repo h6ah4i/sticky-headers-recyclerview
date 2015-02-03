00d308221c308f422773723413e665d47ecd0cbd
Haruki Hasegawa <h6a.h4i.0@gmail.com>
2015-02-03 23:52:15 +0900

    Apply code formatter

--- sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/MainActivity.java
+++ sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/MainActivity.java
@@ -18,75 +18,77 @@ import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersTouchListener;
 
 public class MainActivity extends Activity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-    final SampleArrayHeadersAdapter mAdapter = new SampleArrayHeadersAdapter();
-    String[] animals = getResources().getStringArray(R.array.animals);
-    mAdapter.addAll(animals);
-    recyclerView.setAdapter(mAdapter);
-    int orientation;
-    if (getResources().getConfiguration().orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
-      orientation = LinearLayoutManager.VERTICAL;
-    } else {
-      orientation = LinearLayoutManager.HORIZONTAL;
-    }
-    recyclerView.setLayoutManager(new LinearLayoutManager(this, orientation, false));
-    StickyRecyclerHeadersDecoration headersDecor = new StickyRecyclerHeadersDecoration(mAdapter);
-    recyclerView.addItemDecoration(new DividerDecoration(this));
-    recyclerView.addItemDecoration(headersDecor);
-    StickyRecyclerHeadersTouchListener touchListener =
-        new StickyRecyclerHeadersTouchListener(recyclerView, headersDecor);
-    touchListener.setOnHeaderClickListener(
-        new StickyRecyclerHeadersTouchListener.OnHeaderClickListener() {
-          @Override
-          public void onHeaderClick(View header, int position, long headerId) {
-            Toast.makeText(MainActivity.this, "Header position: " + position + ", id: " + headerId,
-                Toast.LENGTH_SHORT).show();
-          }
-        });
-    recyclerView.addOnItemTouchListener(touchListener);
-    recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
-      @Override
-      public void onItemClick(View view, int position) {
-        mAdapter.remove(mAdapter.getItem(position));
-      }
-    }));
-  }
-
-  private class SampleArrayHeadersAdapter extends RecyclerArrayAdapter<String, RecyclerView.ViewHolder>
-      implements StickyRecyclerHeadersAdapter<RecyclerView.ViewHolder> {
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-      View view = LayoutInflater.from(parent.getContext())
-          .inflate(R.layout.view_item, parent, false);
-      return new RecyclerView.ViewHolder(view) { };
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        final SampleArrayHeadersAdapter mAdapter = new SampleArrayHeadersAdapter();
+        String[] animals = getResources().getStringArray(R.array.animals);
+        mAdapter.addAll(animals);
+        recyclerView.setAdapter(mAdapter);
+        int orientation;
+        if (getResources().getConfiguration().orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+            orientation = LinearLayoutManager.VERTICAL;
+        } else {
+            orientation = LinearLayoutManager.HORIZONTAL;
+        }
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, orientation, false));
+        StickyRecyclerHeadersDecoration headersDecor = new StickyRecyclerHeadersDecoration(mAdapter);
+        recyclerView.addItemDecoration(new DividerDecoration(this));
+        recyclerView.addItemDecoration(headersDecor);
+        StickyRecyclerHeadersTouchListener touchListener =
+                new StickyRecyclerHeadersTouchListener(recyclerView, headersDecor);
+        touchListener.setOnHeaderClickListener(
+                new StickyRecyclerHeadersTouchListener.OnHeaderClickListener() {
+                    @Override
+                    public void onHeaderClick(View header, int position, long headerId) {
+                        Toast.makeText(MainActivity.this, "Header position: " + position + ", id: " + headerId,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        recyclerView.addOnItemTouchListener(touchListener);
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                mAdapter.remove(mAdapter.getItem(position));
+            }
+        }));
     }
 
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-      TextView textView = (TextView) holder.itemView;
-      textView.setText(getItem(position));
-    }
+    private class SampleArrayHeadersAdapter extends RecyclerArrayAdapter<String, RecyclerView.ViewHolder>
+            implements StickyRecyclerHeadersAdapter<RecyclerView.ViewHolder> {
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_item, parent, false);
+            return new RecyclerView.ViewHolder(view) {
+            };
+        }
 
-    @Override
-    public long getHeaderId(int position) {
-      return getItem(position).charAt(0);
-    }
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            textView.setText(getItem(position));
+        }
 
-    @Override
-    public RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
-      View view = LayoutInflater.from(parent.getContext())
-          .inflate(R.layout.view_header, parent, false);
-      return new RecyclerView.ViewHolder(view) { };
-    }
+        @Override
+        public long getHeaderId(int position) {
+            return getItem(position).charAt(0);
+        }
 
-    @Override
-    public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
-      TextView textView = (TextView) holder.itemView;
-      textView.setText(String.valueOf(getItem(position).charAt(0)));
+        @Override
+        public RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_header, parent, false);
+            return new RecyclerView.ViewHolder(view) {
+            };
+        }
+
+        @Override
+        public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            textView.setText(String.valueOf(getItem(position).charAt(0)));
+        }
     }
-  }
 }
--- sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/RecyclerArrayAdapter.java
+++ sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/RecyclerArrayAdapter.java
@@ -8,55 +8,55 @@ import java.util.Collection;
 
 
 public abstract class RecyclerArrayAdapter<M, VH extends RecyclerView.ViewHolder>
-    extends RecyclerView.Adapter<VH> {
-  private ArrayList<M> items = new ArrayList<M>();
+        extends RecyclerView.Adapter<VH> {
+    private ArrayList<M> items = new ArrayList<M>();
 
-  public RecyclerArrayAdapter() {
-    setHasStableIds(true);
-  }
-
-  public void add(M object) {
-    items.add(object);
-    notifyDataSetChanged();
-  }
-
-  public void add(int index, M object) {
-    items.add(index, object);
-    notifyDataSetChanged();
-  }
-
-  public void addAll(Collection<? extends M> collection) {
-    if (collection != null) {
-      items.addAll(collection);
-      notifyDataSetChanged();
+    public RecyclerArrayAdapter() {
+        setHasStableIds(true);
     }
-  }
 
-  public void addAll(M... items) {
-    addAll(Arrays.asList(items));
-  }
+    public void add(M object) {
+        items.add(object);
+        notifyDataSetChanged();
+    }
 
-  public void clear() {
-    items.clear();
-    notifyDataSetChanged();
-  }
+    public void add(int index, M object) {
+        items.add(index, object);
+        notifyDataSetChanged();
+    }
 
-  public void remove(M object) {
-    items.remove(object);
-    notifyDataSetChanged();
-  }
+    public void addAll(Collection<? extends M> collection) {
+        if (collection != null) {
+            items.addAll(collection);
+            notifyDataSetChanged();
+        }
+    }
 
-  public M getItem(int position) {
-    return items.get(position);
-  }
+    public void addAll(M... items) {
+        addAll(Arrays.asList(items));
+    }
 
-  @Override
-  public long getItemId(int position) {
-    return position;
-  }
+    public void clear() {
+        items.clear();
+        notifyDataSetChanged();
+    }
 
-  @Override
-  public int getItemCount() {
-    return items.size();
-  }
+    public void remove(M object) {
+        items.remove(object);
+        notifyDataSetChanged();
+    }
+
+    public M getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
 }
