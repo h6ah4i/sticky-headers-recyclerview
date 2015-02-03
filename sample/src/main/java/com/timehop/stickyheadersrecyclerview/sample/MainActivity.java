d4f2d99e093e4c8fe801046e8db30f10b49c5317
Haruki Hasegawa <h6a.h4i.0@gmail.com>
2015-02-04 01:16:15 +0900

    Modify sample project to support Drag and Drop, swipe operations

--- sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/MainActivity.java
+++ sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/MainActivity.java
@@ -1,41 +1,68 @@
 package com.timehop.stickyheadersrecyclerview.sample;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.method.Touch;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.h6ah4i.android.widget.advrecyclerview.decoration.SimpleListDividerDecorator;
+import com.h6ah4i.android.widget.advrecyclerview.draggable.DraggableItemAdapter;
+import com.h6ah4i.android.widget.advrecyclerview.draggable.RecyclerViewDragDropManager;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.RecyclerViewSwipeManager;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.SwipeableItemAdapter;
+import com.h6ah4i.android.widget.advrecyclerview.touchguard.RecyclerViewTouchActionGuardManager;
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractDraggableSwipeableItemViewHolder;
 import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersAdapter;
 import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
 import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersTouchListener;
 
 
 public class MainActivity extends Activity {
+    RecyclerViewTouchActionGuardManager mRecyclerViewTouchActionGuardManager;
+    RecyclerViewDragDropManager mRecyclerViewDragDropManager;
+    RecyclerViewSwipeManager mRecyclerViewSwipeManager;
+    SampleArrayHeadersAdapter mAdapter;
+    RecyclerView.Adapter mWrappedAdapter;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        final SampleArrayHeadersAdapter mAdapter = new SampleArrayHeadersAdapter();
+
+        mAdapter = new SampleArrayHeadersAdapter();
         String[] animals = getResources().getStringArray(R.array.animals);
         mAdapter.addAll(animals);
-        recyclerView.setAdapter(mAdapter);
-        int orientation;
-        if (getResources().getConfiguration().orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
-            orientation = LinearLayoutManager.VERTICAL;
-        } else {
-            orientation = LinearLayoutManager.HORIZONTAL;
-        }
+
+        mRecyclerViewTouchActionGuardManager = new RecyclerViewTouchActionGuardManager();
+        mRecyclerViewDragDropManager = new RecyclerViewDragDropManager();
+        mRecyclerViewSwipeManager = new RecyclerViewSwipeManager();
+
+        mWrappedAdapter = mRecyclerViewDragDropManager.createWrappedAdapter(mAdapter);
+        mWrappedAdapter = mRecyclerViewSwipeManager.createWrappedAdapter(mWrappedAdapter);
+
+        recyclerView.setAdapter(mWrappedAdapter);
+
+        mRecyclerViewTouchActionGuardManager.attachRecyclerView(recyclerView);
+        mRecyclerViewDragDropManager.attachRecyclerView(recyclerView);
+        mRecyclerViewSwipeManager.attachRecyclerView(recyclerView);
+
+        int orientation = LinearLayoutManager.VERTICAL;
+//        if (getResources().getConfiguration().orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+//            orientation = LinearLayoutManager.VERTICAL;
+//        } else {
+//            orientation = LinearLayoutManager.HORIZONTAL;
+//        }
         recyclerView.setLayoutManager(new LinearLayoutManager(this, orientation, false));
         StickyRecyclerHeadersDecoration headersDecor = new StickyRecyclerHeadersDecoration(mAdapter);
-        recyclerView.addItemDecoration(new DividerDecoration(this));
+//        recyclerView.addItemDecoration(new DividerDecoration(this));
+        recyclerView.addItemDecoration(new SimpleListDividerDecorator(getResources().getDrawable(R.drawable.list_divider), true));
         recyclerView.addItemDecoration(headersDecor);
         StickyRecyclerHeadersTouchListener touchListener =
                 new StickyRecyclerHeadersTouchListener(recyclerView, headersDecor);
@@ -56,20 +83,63 @@ public class MainActivity extends Activity {
         }));
     }
 
-    private class SampleArrayHeadersAdapter extends RecyclerArrayAdapter<String, RecyclerView.ViewHolder>
-            implements StickyRecyclerHeadersAdapter<RecyclerView.ViewHolder> {
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.view_item, parent, false);
-            return new RecyclerView.ViewHolder(view) {
-            };
+    private static class SampleArrayHeadersAdapter extends RecyclerArrayAdapter<String, SampleArrayHeadersAdapter.NormalViewHolder>
+            implements StickyRecyclerHeadersAdapter<SampleArrayHeadersAdapter.HeaderViewHolder>,
+            DraggableItemAdapter<SampleArrayHeadersAdapter.NormalViewHolder>, SwipeableItemAdapter<SampleArrayHeadersAdapter.NormalViewHolder> {
+
+
+        public static class HeaderViewHolder extends RecyclerView.ViewHolder {
+            public HeaderViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
+
+        public static class NormalViewHolder extends AbstractDraggableSwipeableItemViewHolder {
+            public ViewGroup mContainer;
+            public View mDragHandle;
+            public TextView mTextView;
+
+            public NormalViewHolder(View itemView) {
+                super(itemView);
+
+                mContainer = (ViewGroup) itemView.findViewById(R.id.container);
+                mDragHandle = itemView.findViewById(R.id.drag_handle);
+                mTextView = (TextView) ((itemView.getId() == R.id.text) ? itemView : itemView.findViewById(R.id.text));
+            }
+
+            @Override
+            public View getSwipeableContainerView() {
+                return mContainer;
+            }
         }
 
         @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            TextView textView = (TextView) holder.itemView;
-            textView.setText(getItem(position));
+        public NormalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_item, parent, false);
+            return new NormalViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(NormalViewHolder holder, int position) {
+            holder.mTextView.setText(getItem(position));
+
+            final int dragState = holder.getDragStateFlags();
+            final int swipeState = holder.getSwipeStateFlags();
+            if (((dragState & RecyclerViewDragDropManager.STATE_FLAG_IS_UPDATED) != 0) ||
+                ((swipeState & RecyclerViewDragDropManager.STATE_FLAG_IS_UPDATED) != 0)) {
+                if ((dragState & RecyclerViewDragDropManager.STATE_FLAG_IS_ACTIVE) != 0) {
+                    holder.mContainer.setBackgroundColor(0x80ff0000);
+                } else if ((dragState & RecyclerViewDragDropManager.STATE_FLAG_DRAGGING) != 0) {
+                    holder.mContainer.setBackgroundColor(0x40ff0000);
+                } else if ((swipeState & RecyclerViewSwipeManager.STATE_FLAG_IS_ACTIVE) != 0) {
+                    holder.mContainer.setBackgroundColor(0xffffffff);
+                } else if ((swipeState & RecyclerViewSwipeManager.STATE_FLAG_SWIPING) != 0) {
+                    holder.mContainer.setBackgroundColor(0xfff0f0ff);
+                } else {
+                    holder.mContainer.setBackgroundColor(0xffffffff);
+                }
+            }
         }
 
         @Override
@@ -78,17 +148,100 @@ public class MainActivity extends Activity {
         }
 
         @Override
-        public RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+        public HeaderViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
             View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.view_header, parent, false);
-            return new RecyclerView.ViewHolder(view) {
-            };
+            return new HeaderViewHolder(view);
         }
 
         @Override
-        public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
+        public void onBindHeaderViewHolder(HeaderViewHolder holder, int position) {
             TextView textView = (TextView) holder.itemView;
             textView.setText(String.valueOf(getItem(position).charAt(0)));
+        }
+
+        @Override
+        public boolean onCheckCanStartDrag(NormalViewHolder holder, int x, int y) {
+            if (holder.mContainer == null || holder.mDragHandle == null) {
+                return false;
+            }
+
+            // x, y --- relative from the itemView's top-left
+            final View containerView = holder.mContainer;
+            final View dragHandleView = holder.mDragHandle;
+
+            final int offsetX = containerView.getLeft() + (int) (ViewCompat.getTranslationX(containerView) + 0.5f);
+            final int offsetY = containerView.getTop() + (int) (ViewCompat.getTranslationY(containerView) + 0.5f);
+
+            return hitTest(dragHandleView, x - offsetX, y - offsetY);
+        }
+
+        @Override
+        public void onMoveItem(int fromPosition, int toPosition) {
+            move(fromPosition, toPosition);
+        }
+
+        @Override
+        public int onGetSwipeReactionType(NormalViewHolder holder, int x, int y) {
+            boolean canSwipe = false;
+
+            if (holder.mContainer != null) {
+                if (!onCheckCanStartDrag(holder, x, y)) {
+                    canSwipe = true;
+                }
+            }
+
+            if (canSwipe) {
+                return RecyclerViewSwipeManager.REACTION_CAN_SWIPE_LEFT |
+                        RecyclerViewSwipeManager.REACTION_CAN_SWIPE_RIGHT;
+            } else {
+                return RecyclerViewSwipeManager.REACTION_CAN_NOT_SWIPE_LEFT |
+                        RecyclerViewSwipeManager.REACTION_CAN_NOT_SWIPE_RIGHT;
+            }
+        }
+
+        @Override
+        public void onSetSwipeBackground(NormalViewHolder holder, int type) {
+            switch (type) {
+                case RecyclerViewSwipeManager.DRAWABLE_SWIPE_NEUTRAL_BACKGROUND:
+                    holder.itemView.setBackgroundColor(0x00000000);
+                    break;
+                case RecyclerViewSwipeManager.DRAWABLE_SWIPE_LEFT_BACKGROUND:
+                    holder.itemView.setBackgroundColor(0xff008800);
+                    break;
+                case RecyclerViewSwipeManager.DRAWABLE_SWIPE_RIGHT_BACKGROUND:
+                    holder.itemView.setBackgroundColor(0xffcc0000);
+                    break;
+            }
+        }
+
+        @Override
+        public int onSwipeItem(NormalViewHolder holder, int result) {
+            switch (result) {
+                case RecyclerViewSwipeManager.RESULT_SWIPED_LEFT:
+                case RecyclerViewSwipeManager.RESULT_SWIPED_RIGHT:
+                    return RecyclerViewSwipeManager.AFTER_SWIPE_REACTION_REMOVE_ITEM;
+                default:
+                    return RecyclerViewSwipeManager.AFTER_SWIPE_REACTION_DEFAULT;
+            }
+        }
+
+        @Override
+        public void onPerformAfterSwipeReaction(NormalViewHolder holder, int result, int reaction) {
+            if (reaction == RecyclerViewSwipeManager.AFTER_SWIPE_REACTION_REMOVE_ITEM) {
+                remove(getItem(holder.getPosition()));
+            }
+        }
+
+        public static boolean hitTest(View v, int x, int y) {
+            final int tx = (int) (ViewCompat.getTranslationX(v) + 0.5f);
+            final int ty = (int) (ViewCompat.getTranslationY(v) + 0.5f);
+            final int left = v.getLeft() + tx;
+            final int right = v.getRight() + tx;
+            final int top = v.getTop() + ty;
+            final int bottom = v.getBottom() + ty;
+
+            return (x >= left) && (x <= right) && (y >= top) && (y <= bottom);
         }
     }
 }
--- sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/RecyclerArrayAdapter.java
+++ sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/RecyclerArrayAdapter.java
@@ -1,5 +1,6 @@
 package com.timehop.stickyheadersrecyclerview.sample;
 
+import android.support.v4.util.Pair;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -9,25 +10,28 @@ import java.util.Collection;
 
 public abstract class RecyclerArrayAdapter<M, VH extends RecyclerView.ViewHolder>
         extends RecyclerView.Adapter<VH> {
-    private ArrayList<M> items = new ArrayList<M>();
+    private ArrayList<Pair<Long, M>> items = new ArrayList<Pair<Long, M>>();
+    private long idCounter = 0;
 
     public RecyclerArrayAdapter() {
         setHasStableIds(true);
     }
 
     public void add(M object) {
-        items.add(object);
+        items.add(new Pair<Long, M>(idCounter++, object));
         notifyDataSetChanged();
     }
 
     public void add(int index, M object) {
-        items.add(index, object);
+        items.add(index, new Pair<Long, M>(idCounter++, object));
         notifyDataSetChanged();
     }
 
     public void addAll(Collection<? extends M> collection) {
         if (collection != null) {
-            items.addAll(collection);
+            for (M x : collection) {
+                items.add(new Pair<Long, M>(idCounter++, x));
+            }
             notifyDataSetChanged();
         }
     }
@@ -42,17 +46,40 @@ public abstract class RecyclerArrayAdapter<M, VH extends RecyclerView.ViewHolder
     }
 
     public void remove(M object) {
-        items.remove(object);
+        int foundIndex = -1;
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).second == object) {
+                foundIndex = i;
+                break;
+            }
+        }
+
+        if (foundIndex < 0) {
+            return;
+        }
+
+        items.remove(foundIndex);
+//        notifyItemRemoved(foundIndex);
         notifyDataSetChanged();
     }
 
+    public void move(int fromPosition, int toPosition) {
+        if (fromPosition == toPosition) {
+            return;
+        }
+
+        Pair<Long, M> item = items.remove(fromPosition);
+        items.add(toPosition, item);
+        notifyItemMoved(fromPosition, toPosition);
+    }
+
     public M getItem(int position) {
-        return items.get(position);
+        return items.get(position).second;
     }
 
     @Override
     public long getItemId(int position) {
-        return position;
+        return items.get(position).first;
     }
 
     @Override
